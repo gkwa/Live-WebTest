8856a47fd2d0bee9fb3e60c994b2d62d2d55a984
Hiro797 <Onewolf7010>
2014-07-28 17:30:43 -0700

    updated

--- AccountImfowithmaven/src/SystemAdminLivedev3Account.java
+++ AccountImfowithmaven/src/SystemAdminLivedev3Account.java
@@ -1,6 +1,8 @@
 
 
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.firefox.FirefoxDriver;
 
 
@@ -26,6 +28,11 @@ public class SystemAdminLivedev3Account extends AccountPage{
 		//obj.SetWebSite("http://d2.streambox.com/ls/SLSLogin.php");
 		obj.Login("HiroSysAdmin","demo");
 		obj.run();
+		
+		_Create_otherAccount();
+		
+		
+		
 		}catch(Exception e){
 			
 			System.out.println("SysAdministrator Error");
@@ -36,6 +43,69 @@ public class SystemAdminLivedev3Account extends AccountPage{
 		
 	}
 	
+	void _Create_otherAccount(){
+		
+		driver.findElement(By.xpath("//div[@id='menu']/div[@class = 'navbar-inner']/div[@class = 'container-fluid']/div/div[@id = 'ctn_nav']/ul[@class = 'gradient level1 nav']/li[@class = 'dropdown']/a[@class = 'dropdown-toggle']")).click();
+		
+		//if(<=0)
+		
+		//driver.findElement(By.xpath("//div[@id='menu']/div[@class = 'navbar-inner']/div[@class = 'container-fluid']/div/div[@id = 'ctn_nav']/ul[@class = 'gradient level1 nav']/li[@class = 'dropdown open']/ul[@class = 'dropdown-menu']/li[@class = 'active']/a[@id = 'current']")).click();
+		
+		driver.findElement(By.xpath("//div[@id='menu']/div[@class = 'navbar-inner']/div[@class = 'container-fluid']/div/div[@id = 'ctn_nav']/ul[@class = 'gradient level1 nav']/li[@class = 'dropdown open']/ul[@class = 'dropdown-menu']/li/a[@href = 'slsusers.php']")).click();
+		
+		//************************Add Account**********************************************//
+		driver.findElement(By.xpath("//div[@id='container']/div[@class = 'container-fluid']/ul[@id = 'submenu']/li[@id = 'submenu_addNewUser']/a[@class = 'submenu_control btn']")).click();
+		
+		
+		
+		//***************************Set up Login*********************************//
+		WebElement element1 = driver.findElement(By.xpath("//p[@class='controls']/input[@id='uname1']"));
+		element1.clear();
+		element1.sendKeys("hiro1GroupAdmin");
+		//***************************End Set up Login*********************************//
+		
+
+		//***************************Set up Pass*********************************//
+		WebElement element2 = driver.findElement(By.xpath("//p[@class='controls']/input[@id='upass1']"));
+		element2.clear();
+		element2.sendKeys("demoGroupAdmin");
+		//***************************End Set up Pass*********************************//
+		
+
+		//***************************Set up Pass*********************************//
+		WebElement element2s = driver.findElement(By.xpath("//p[@class='controls']/input[@id='upass2']"));
+		element2s.clear();
+		element2s.sendKeys("demoGroupAdmin");
+		//***************************End Set up Pass*********************************//
+
+		
+		//***************************Set up Group*********************************//
+		WebElement element3 = driver.findElement(By.xpath("//p[@class='controls']/input[@id='udrm1']"));
+		element3.clear();
+		element3.sendKeys("hiro1");
+		//***************************End Set up Group*********************************//
+		
+		obj.To_ChangeConfirmAccount(driver);
+		
+		
+		
+
+		//************************Show Account**********************************************//
+		driver.findElement(By.xpath("//div[@id='container']/div[@class = 'container-fluid']/ul[@id = 'submenu']/li[@id = 'submenu_getRole']/a[@class = 'submenu_control btn']")).click();
+		driver.findElement(By.xpath("//div[@id='container']/div[@class = 'container-fluid']/ul[@id = 'submenu']/li[@id = 'submenu_getUser']/a[@class = 'submenu_control btn']")).click();
+	
+		//*************************Serchbox****************************************//
+		WebElement elementsearch = driver.findElement(By.xpath("//div[@id='menu']/div[@class = 'navbar-inner']/div[@class = 'container-fluid']/div/div[@class='navbar-form pull-right']/div[@id='searchBar']/form[@class = 'navbar-form input-append']/input[@id='flt']"));
+		elementsearch.clear();
+		elementsearch.sendKeys("hiro1");
+		
+		
+		
+	}
+	
+	
+	
+	
 	
 
-}
+}//end of the class of system admin
